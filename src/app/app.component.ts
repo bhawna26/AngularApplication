490d386a3a99bbfe99df2120f7d7ba004356a1be
Bhawna <71230093+bhawna26@users.noreply.github.com>
2024-05-07 12:04:45 +0530

    Data binding from .ts to .html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todoApplication';
+  // message is binded to htmlfile using two curly brackets
   message="hello user";
 }
