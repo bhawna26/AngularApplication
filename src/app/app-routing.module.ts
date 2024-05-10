4b883e9c18e5d901c5a3f08ca625a1566026954a
Bhawna <71230093+bhawna26@users.noreply.github.com>
2024-05-10 10:01:07 +0530

    Routing in angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ErrorcomponentComponent } from './errorcomponent/errorcomponent.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:LoginComponent},
+  {path:'login',component:LoginComponent},
+  {path:'welcome',component:WelcomeComponent},
+  //errorcomponent
+  {path:'**',component:ErrorcomponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
