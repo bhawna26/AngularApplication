60096ba002f38cca92c8aef8e73b35f2f3fdf01e
Bhawna <71230093+bhawna26@users.noreply.github.com>
2024-05-28 10:20:29 +0530

    Adding boostrap

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+export class Todo{
+  constructor(
+    public id:number,
+    public description:string,
+    public done:boolean,
+    public targetDate:Date
+  ){
 
+  }
+}
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -7,26 +16,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListTodosComponent implements OnInit{
   todos=[
-    {
-      id:1,
-      description:'Learn to dance'
-    },
-    {
-      id:2,
-      description:'workout'
+    new Todo(1,"Learn to dance",false,new Date()),
+    new Todo(2,"Become an expert at angular",false,new Date()),
+    new Todo(3,"Become an expert at react",false,new Date()),
+    new Todo(4,"Workout",true,new Date()),
 
-    },
-    {
-      id:3,
-      description:'study'
-
-    },
-  
   ]
-  // todo={
-  //   id:1,
-  //   description:'Learn to dance'
-  // }
   constructor(){
 
   }
