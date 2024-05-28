40c15cf4b5811cafcecfbce5752a3c62609e848c
Bhawna <71230093+bhawna26@users.noreply.github.com>
2024-05-28 10:26:57 +0530

    Resolving method errors

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,32 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-export class Todo{
-  constructor(
-    public id:number,
-    public description:string,
-    public done:boolean,
-    public targetDate:Date
-  ){
 
-  }
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ) {}
 }
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
-  styleUrl: './list-todos.component.css'
+  styleUrls: ['./list-todos.component.css'] // Fix: Use 'styleUrls' instead of 'styleUrl'
 })
-export class ListTodosComponent implements OnInit{
-  todos=[
-    new Todo(1,"Learn to dance",false,new Date()),
-    new Todo(2,"Become an expert at angular",false,new Date()),
-    new Todo(3,"Become an expert at react",false,new Date()),
-    new Todo(4,"Workout",true,new Date()),
+export class ListTodosComponent implements OnInit {
+  todos = [
+    new Todo(1, "Learn to dance", false, new Date()),
+    new Todo(2, "Become an expert at angular", false, new Date()),
+    new Todo(3, "Become an expert at react", false, new Date()),
+    new Todo(4, "Workout", true, new Date()),
+  ];
 
-  ]
-  constructor(){
+  constructor() {}
 
+  ngOnInit(): void {
+    // Initialization logic here
   }
-ngOnInit(): void {
-  throw new Error('Method not implemented.');
-}
-
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,25 +4,28 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-ngOnInit(): void {
-  throw new Error('Method not implemented.');
-}
-username='bhawna'
-password='123'
-errorMessage='Invalid Credentials'
-invalidLogin=false
-//Router by dependency injection.Router is a dependency of the login component.
-constructor(private router:Router){
-  
-}
-handleLogin(){
-  if(this.username==='bhawna' && this.password==='123'){
-    this.invalidLogin=false;
-    this.router.navigate(['welcome',this.username]);
+
+  username = 'bhawna';
+  password = '123';
+  errorMessage = 'Invalid Credentials';
+  invalidLogin = false;
+
+  // Router by dependency injection. Router is a dependency of the login component.
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    // Any initialization logic can be added here
   }
-  else this.invalidLogin=true;
-}
+
+  handleLogin(): void {
+    if (this.username === 'bhawna' && this.password === '123') {
+      this.invalidLogin = false;
+      this.router.navigate(['welcome', this.username]);
+    } else {
+      this.invalidLogin = true;
+    }
+  }
 }
