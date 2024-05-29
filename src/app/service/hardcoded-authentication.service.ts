09194fad635f0f89a08c380bbbf062471861338f
Bhawna <71230093+bhawna26@users.noreply.github.com>
2024-05-29 12:06:32 +0530

    Session storage and enabling menu links based on user authentication token

--- src/app/service/hardcoded-authentication.service.ts
+++ src/app/service/hardcoded-authentication.service.ts
@@ -8,9 +8,17 @@ export class HardcodedAuthenticationService {
   constructor() { }
 
   authenticate(username: string, password: string): boolean {
+    console.log('before'+this.isUserLoggedIn());
     if (username === 'bhawna' && password === '123') {
+      sessionStorage.setItem('authenticateUser',username);
+      console.log('after'+this.isUserLoggedIn());
+
       return true;
     }
     return false;
+  }
+  isUserLoggedIn(){
+    let user=sessionStorage.getItem('authenticateUser')
+    return !(user==null)
   }
 }
